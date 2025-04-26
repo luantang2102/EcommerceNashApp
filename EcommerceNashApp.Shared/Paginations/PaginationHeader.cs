953b5c0640a979c2d77347205b6582cb40cf3948
LuanTang <luantang.work@gmail.com>
2025-04-27 02:14:56 +0700

    + Added PaginationHeader class

---
+++ EcommerceNashApp.Shared/Paginations/PaginationHeader.cs
@@ -0,0 +1,10 @@
+﻿namespace EcommerceNashApp.Shared.Paginations
+{
+    public class PaginationHeader
+    {
+        public int TotalCount { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
