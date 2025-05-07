4c0a516037f3896636bd18f28f978d5a38b87d42
LuanTang <luantang.work@gmail.com>
2025-05-07 13:45:17 +0700

    + PaginationService in Shared Library

--- EcommerceNashApp.Shared/Paginations/PagedList.cs
+++ EcommerceNashApp.Shared/Paginations/PagedList.cs
@@ -17,12 +17,5 @@ namespace EcommerceNashApp.Shared.Paginations
         }
 
         public PaginationMetadata Metadata { get; set; }
-
-        public static async Task<PagedList<T>> ToPagedList(IQueryable<T> query, int pageNumber, int pageSize)
-        {
-            var count = await query.CountAsync();
-            var items = await query.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
-            return new PagedList<T>(items, count, pageNumber, pageSize);
-        }
     }
 }
---
+++ EcommerceNashApp.Shared/Paginations/Service/IPaginationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceNashApp.Shared.Paginations.Service
+{
+    public interface IPaginationService
+    {
+        Task<PagedList<T>> EF_ToPagedList<T>(IQueryable<T> query, int pageNumber, int pageSize);
+    }
+}
